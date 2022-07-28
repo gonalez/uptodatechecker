865765c266e94571f291d03ca0c5ab115f9bd4ce
Gonalez <gastongaga202@gmail.com>
2022-07-28 15:09:10 -0300

    Move class

--- src/main/java/io/github/gonalez/uptodatechecker/ExecutorFutureScheduler.java
+++ src/main/java/io/github/gonalez/uptodatechecker/ExecutorFutureScheduler.java
@@ -20,6 +20,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * A basic implementation of {@link FutureScheduler} using a {@code ExecutorService}.
@@ -38,5 +39,54 @@ public class ExecutorFutureScheduler implements FutureScheduler {
     RepeatingCallable<V> repeatingCallable = new RepeatingCallable<>(callable, period, timeUnit);
     executor.submit(repeatingCallable);
     return repeatingCallable;
+  }
+  
+  /**
+   * A {@link Callable} which can be repeated multiple times until is
+   * {@link Cancellable#cancel() cancelled}, this implementation is synchronous.
+   *
+   * <p>{@code callable} is the default callable to be called on each repetition,
+   * we wait {@code period} using the {@code timeUnit} to go for the next repetition.
+   */
+  private static final class RepeatingCallable<V> implements Callable<V>, Cancellable {
+    private final Callable<V> callable;
+    private final long period;
+    private final TimeUnit timeUnit;
+    
+    private final AtomicBoolean cancelled = new AtomicBoolean(false);
+    
+    public RepeatingCallable(
+        Callable<V> callable,
+        long period,
+        TimeUnit timeUnit) {
+      this.callable = callable;
+      this.period = period;
+      this.timeUnit = timeUnit;
+    }
+    
+    @Override
+    public V call() throws Exception {
+      V call = callable.call();
+      while (true) {
+        if (cancelled.get()) {
+          break;
+        }
+        try {
+          if (Thread.interrupted()) {
+            throw new InterruptedException();
+          }
+          timeUnit.sleep(period);
+          call = callable.call();
+        } catch (Exception exception) {
+          break;
+        }
+      }
+      return call;
+    }
+    
+    @Override
+    public void cancel() {
+      cancelled.compareAndSet(false, true);
+    }
   }
 }
