fb8d581e49820063efad0731842765732298e4db
Gonalez <gastongaga202@gmail.com>
2022-08-03 03:06:13 -0300

    Add package-info.java

---
+++ src/main/java/io/github/gonalez/uptodatechecker/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2022 - Gaston Gonzalez (Gonalez)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This package contains the core API for the UpToDateChecker library.
+ *
+ * <p>The entry point for the API is the {@link io.github.gonalez.uptodatechecker.UpToDateChecker},
+ * additionally {@link io.github.gonalez.uptodatechecker.FluentUpToDateCheckerCall} can be used to
+ * interact with the API in an easier way, although this is not recommended.
+ */
+package io.github.gonalez.uptodatechecker;
